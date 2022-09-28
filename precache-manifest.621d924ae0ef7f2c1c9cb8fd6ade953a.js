6bab2265f9a3c16cd7713b20799709414006e23a
MorozZz <pux.88@mail.ru>
2022-09-28 22:50:30 +0300

    Updates

---
+++ precache-manifest.621d924ae0ef7f2c1c9cb8fd6ade953a.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "63c0ac864a34255acbc7",
+    "url": "/alex/static/css/main.a2cc5a81.chunk.css"
+  },
+  {
+    "revision": "63c0ac864a34255acbc7",
+    "url": "/alex/static/js/main.63c0ac86.chunk.js"
+  },
+  {
+    "revision": "48dc1c1c87d8faf3dbc4",
+    "url": "/alex/static/js/1.48dc1c1c.chunk.js"
+  },
+  {
+    "revision": "ba78913e8ebaf50befb5",
+    "url": "/alex/static/js/runtime~main.ba78913e.js"
+  },
+  {
+    "revision": "c37c9b9ede6eaed64e784537f15a750f",
+    "url": "/alex/static/media/Group 270988500.c37c9b9e.svg"
+  },
+  {
+    "revision": "35300b66623fb653c31e133e4ed2ffc2",
+    "url": "/alex/static/media/Group 270988514.35300b66.svg"
+  },
+  {
+    "revision": "d3bea51a69566a28f37d5d2bde052888",
+    "url": "/alex/static/media/Vector.d3bea51a.svg"
+  },
+  {
+    "revision": "a31c620d04b8df473471ff8ca322fa1c",
+    "url": "/alex/static/media/Vector (1).a31c620d.svg"
+  },
+  {
+    "revision": "393639c1d1596a6a70fe66bd38bab15b",
+    "url": "/alex/static/media/Vector (2).393639c1.svg"
+  },
+  {
+    "revision": "69d76375369dfdcb313ac54336281de0",
+    "url": "/alex/static/media/Vector (3).69d76375.svg"
+  },
+  {
+    "revision": "09d3ef5954fa962743c0508514702f9f",
+    "url": "/alex/static/media/ic_prd_mobileapp_default_20_w.09d3ef59.svg"
+  },
+  {
+    "revision": "f655a845638ede0e6b77bfbc6d82d9cc",
+    "url": "/alex/static/media/Union (1).f655a845.svg"
+  },
+  {
+    "revision": "aebb7d3c9299ef35ead55210f086e3b2",
+    "url": "/alex/static/media/Home.aebb7d3c.svg"
+  },
+  {
+    "revision": "174c1b26a8e85914da48ee15ac628f9b",
+    "url": "/alex/static/media/Union (2).174c1b26.svg"
+  },
+  {
+    "revision": "021ea0ba59acbe7ced782209d6fdf419",
+    "url": "/alex/static/media/Plus.021ea0ba.svg"
+  },
+  {
+    "revision": "fd12cdcec70ffeec229bb7bd1122601d",
+    "url": "/alex/static/media/Plus (2).fd12cdce.svg"
+  },
+  {
+    "revision": "fa306def932eb12d68ceb57ac2b262f4",
+    "url": "/alex/static/media/Wallet.fa306def.svg"
+  },
+  {
+    "revision": "aa963a3e30ec6b108226d7d5b3e0bf4e",
+    "url": "/alex/static/media/Union.aa963a3e.svg"
+  },
+  {
+    "revision": "942f23cc92b7f223d79b20538541cb8d",
+    "url": "/alex/static/media/Plus (1).942f23cc.svg"
+  },
+  {
+    "revision": "ed5d1aaeb0e6fbcdd34112b8c94135ed",
+    "url": "/alex/index.html"
+  }
+];
